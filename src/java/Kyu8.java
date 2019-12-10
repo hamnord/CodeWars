0e1cac0e104c738ebe0373553b0553f45549d5e4
hamnord <h.nordenstein@hotmail.com>
2019-12-10 11:12:11 +0100

    added a 8kyu

--- src/java/Kyu8.java
+++ src/java/Kyu8.java
@@ -62,5 +62,11 @@ public class Kyu8 {
     return decimal;
   }
 
+  public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+    // your code here
+    int sum = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    return sum;
+  }
+
 }
 
